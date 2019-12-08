1354e1760ba0b5fc0589e12d3f7ad346f0d9e940
Patrick Horne <patrickahorne@gmail.com>
2019-12-09 03:16:35 +1100

    Try writing something that reads stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+// https://doc.rust-lang.org/std/io/fn.stdin.html
+fn main() -> io::Result<()>  {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    Ok(println!());
 }
