328475aaae973ab4e2e664da62a07efb872facef
Patrick Horne <patrickahorne@gmail.com>
2019-12-11 10:20:51 +1100

    Capture stdin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use std::io::{self, Read};
 
 // https://doc.rust-lang.org/std/io/fn.stdin.html
-fn main() -> io::Result<()>  {
+fn main() {
+    let input = read_stdin()
+        .unwrap()
+        .to_string();
+
+    println!("{}", input);
+}
+
+fn read_stdin() -> io::Result<String> {
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer)?;
-    Ok(println!());
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn write_stdout() {
+
 }
