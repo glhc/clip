3a92f85534aa3083125b6b4671087d7e5e160a86
Patrick Horne <patrickahorne@gmail.com>
2019-12-11 17:09:37 +1100

    Accept stdin and pass that data to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 
 // https://doc.rust-lang.org/std/io/fn.stdin.html
 fn main() {
     let input = read_stdin()
         .unwrap()
         .to_string();
-
-    println!("{}", input);
+    
+    let write_result = write_stdout(input);
+    assert_eq!(write_result.is_err(), false);
+    
 }
 
 fn read_stdin() -> io::Result<String> {
@@ -15,9 +17,16 @@ fn read_stdin() -> io::Result<String> {
     let mut handle = stdin.lock();
 
     handle.read_to_string(&mut buffer)?;
+
     Ok(buffer)
 }
 
-fn write_stdout() {
+fn write_stdout(input: String) -> io::Result<()> {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    let output = input.into_bytes();
+    
+    handle.write_all(&output);
 
+    Ok(())
 }
